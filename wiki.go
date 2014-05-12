377d5aa00f66cab5ff6bac7056992a5c5be07d1e
manuchandraprasad <manu@quadloops.com>
2014-05-13 02:42:09 +0530

    Structured the whole directory

--- wiki.go
+++ wiki.go
@@ -18,12 +18,12 @@ type Page struct {
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+	return ioutil.WriteFile("wikis/"+filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
+	body, err := ioutil.ReadFile("wikis/" + filename)
 	if err != nil {
 		return nil, err
 	}
@@ -31,7 +31,7 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
+	t, _ := template.ParseFiles("templates/" + tmpl + ".html")
 	t.Execute(w, p)
 }
 
@@ -39,14 +39,17 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	//So GOLang give you nil errors if there is something wrong with templates
 	// http: panic serving 127.0.0.1:58332: runtime error: invalid memory address or nil pointer dereference
 	title := r.URL.Path[len("/view/"):]
-	fmt.Printf("%s", r.URL.Path)
-	p, _ := loadPage(title)
+	fmt.Printf("%s\n", r.URL.Path)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+	}
 	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
-	fmt.Printf("%s", r.URL.Path)
+	fmt.Printf("%s\n", r.URL.Path)
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -54,9 +57,17 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
